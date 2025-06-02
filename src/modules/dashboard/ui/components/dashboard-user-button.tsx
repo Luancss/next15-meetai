ffe0d941b992d4f20e88334514d92f703ec00a7d
Luan <ventobo123@gmail.com>
2025-06-02 14:50:45 -0300

    feat: refactor DashboardUserButton to enhance dropdown menu structure and styling

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,8 +1,15 @@
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { DropdownMenu } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
 import { authClient } from "@/lib/auth-client";
 import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
 
 export const DashboardUserButton = () => {
   const { data, isPending } = authClient.useSession();
@@ -27,9 +34,29 @@ export const DashboardUserButton = () => {
         )}
         <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
           <p className="text-sm truncate w-full">{data.user.name}</p>
-          <p className="text-sm truncate w-full">{data.user.email}</p>
+          <p className="text-xs truncate w-full">{data.user.email}</p>
         </div>
+        <ChevronDownIcon className="size-4 shrink-0" />
       </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" side="right" className="w-72">
+        <DropdownMenuLabel>
+          <div className="flex flex-col gap-1">
+            <span className="font-medium truncate">{data.user.name}</span>
+            <span className="text-sm font-normal text-muted-foreground truncate">
+              {data.user.email}
+            </span>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="cursor-pointer flex items-center justify-between">
+          Billing
+          <CreditCardIcon className="size-4" />
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer flex items-center justify-between">
+          Logout
+          <LogOutIcon className="size-4" />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
