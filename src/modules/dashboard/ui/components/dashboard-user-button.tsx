12dfdab29b0bad7c2222715407477c0074a9c767
Luan <ventobo123@gmail.com>
2025-06-02 14:31:32 -0300

    feat: implement GeneratedAvatar component for user profile display

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,3 +1,4 @@
+import { GeneratedAvatar } from "@/components/generated-avatar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { DropdownMenu } from "@/components/ui/dropdown-menu";
 import { authClient } from "@/lib/auth-client";
@@ -12,12 +13,22 @@ export const DashboardUserButton = () => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-2">
         {data.user.image ? (
           <Avatar>
             <AvatarImage src={data.user.image} />
           </Avatar>
-        ) : null}
+        ) : (
+          <GeneratedAvatar
+            seed={data.user.name}
+            variant="initials"
+            className="size-9 mr-3"
+          />
+        )}
+        <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+          <p className="text-sm truncate w-full">{data.user.name}</p>
+          <p className="text-sm truncate w-full">{data.user.email}</p>
+        </div>
       </DropdownMenuTrigger>
     </DropdownMenu>
   );
