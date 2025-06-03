16d2cfe8663685bf0d438c8a917805db5c271bfc
Luan <ventobo123@gmail.com>
2025-06-03 16:28:04 -0300

    feat: remove artificial delay from getMany procedure in agentsRouter

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -6,8 +6,6 @@ export const agentsRouter = createTRPCRouter({
   getMany: baseProcedure.query(async () => {
     const data = db.select().from(agents);
 
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-
     return data;
   }),
 });
