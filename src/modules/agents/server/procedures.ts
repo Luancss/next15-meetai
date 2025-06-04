cb710ead5d6edd01744c477db605c40cc4d3dcbf
Luan <ventobo123@gmail.com>
2025-06-04 14:41:48 -0300

    feat: update agentsRouter to include pagination and search functionality; add constants for default page settings; adjust query options in components

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,9 +1,15 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { eq, getTableColumns, sql } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { z } from "zod";
 import { agentsInsertSchema } from "../schemas";
+import {
+  DEFAULT_PAGE,
+  DEFAULT_PAGE_SIZE,
+  MAX_PAGE_SIZE,
+  MIN_PAGE_SIZE,
+} from "@/constants";
 
 export const agentsRouter = createTRPCRouter({
   // TODO: Change 'getOne' to use 'protectedProcecure'
@@ -24,24 +30,53 @@ export const agentsRouter = createTRPCRouter({
   // TODO: Change 'getMany' to use 'protectedProcecure'
   getMany: protectedProcedure
     .input(
-      z
-        .object({
-          page: z.number().min(1).default(1),
-          pageSize: z.number().min(1).max(100).default(10),
-          search: z.string().nullish(),
-        })
-        .optional()
+      z.object({
+        page: z.number().min(1).default(DEFAULT_PAGE),
+        pageSize: z
+          .number()
+          .min(MIN_PAGE_SIZE)
+          .max(MAX_PAGE_SIZE)
+          .default(DEFAULT_PAGE_SIZE),
+        search: z.string().nullish(),
+      })
     )
-    .query(async () => {
+    .query(async ({ ctx, input }) => {
+      const { search, page, pageSize } = input;
+
       const data = db
         .select({
           // TODO: Change to actual count of meetings
           meetingCount: sql<number>`5`,
           ...getTableColumns(agents),
         })
-        .from(agents);
+        .from(agents)
+        .where(
+          and(
+            eq(agents.userId, ctx.auth.user.id),
+            search ? ilike(agents.name, `%${search}%`) : undefined
+          )
+        )
+        .orderBy(desc(agents.createdAt), desc(agents.id))
+        .limit(pageSize)
+        .offset((page - 1) * pageSize);
 
-      return data;
+      const [total] = await db
+        .select({ count: count() })
+        .from(agents)
+        .where(
+          and(
+            eq(agents.userId, ctx.auth.user.id),
+            search ? ilike(agents.name, `%${search}%`) : undefined
+          )
+        );
+
+      const totalPages = Math.ceil(total.count / pageSize);
+
+      return {
+        items: data,
+        total: total.count,
+        totalPages,
+      };
     }),
   create: protectedProcedure
     .input(agentsInsertSchema)
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -19,7 +19,7 @@ const mockData: Payment[] = [
 
 export const AgentsView = () => {
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({}));
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
