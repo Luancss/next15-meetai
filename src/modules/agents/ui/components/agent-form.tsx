6322dd72855931b1250d4b63796a3cbf96840fca
Luan <ventobo123@gmail.com>
2025-06-03 23:51:30 -0300

    feat: update AgentForm to use AgentGetOne type and implement form submission logic

--- src/modules/agents/ui/components/agent-form.tsx
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -1,6 +1,82 @@
+import { useTRPC } from "@/trpc/client";
+import { AgentGetOne } from "../../types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { agentsInsertSchema } from "../../schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { Input } from "@/components/ui/input";
+
 interface AgentFormProps {
   onSucces?: () => void;
   onCancel?: () => void;
-  initialValues?: any;
+  initialValues?: AgentGetOne;
 }
-export const AgentForm = () => {};
+export const AgentForm = ({
+  onSucces,
+  onCancel,
+  initialValues,
+}: AgentFormProps) => {
+  const trpc = useTRPC();
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const createAgent = useMutation(
+    trpc.agents.create.mutationOptions({
+      onSuccess: () => {},
+      onError: () => {},
+    })
+  );
+
+  const form = useForm<z.infer<typeof agentsInsertSchema>>({
+    resolver: zodResolver(agentsInsertSchema),
+    defaultValues: {
+      name: initialValues?.name || "",
+      instructions: initialValues?.instructions || "",
+    },
+  });
+
+  const isEdit = !!initialValues?.id;
+  const isPending = createAgent.isPending;
+
+  const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
+    if (isEdit) {
+      console.log("TODO: Implement agent update logic'");
+    } else {
+      createAgent.mutate(values);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <GeneratedAvatar
+          seed={form.watch("name")}
+          variant="botttsNeutral"
+          className="border size-16"
+        />
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
