14885a75f36fc6231b81c654f18899663c6d5136
Luan <ventobo123@gmail.com>
2025-06-04 11:56:56 -0300

    feat: integrate DataTable component in AgentsView for displaying agent data

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,12 +4,27 @@ import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { columns, Payment } from "../components/columns";
+import { DataTable } from "../components/data-table";
+
+const mockData: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+];
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+    </div>
+  );
 };
 
 export const AgentsViewLoading = () => {
