0752efc01ae679385fb966d4ef5b1db6b3e366b5
Luan <ventobo123@gmail.com>
2025-09-24 23:39:05 -0300

    feat: create Layout component and update CallView to handle meeting status

--- src/modules/call/ui/views/call-view.tsx
+++ src/modules/call/ui/views/call-view.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ErrorState } from "@/components/error-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
@@ -10,8 +11,19 @@ interface Props {
 export const CallView = ({ meetingId }: Props) => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(
-    trpc.agents.getOne.queryOptions({ id: meetingId })
+    trpc.meetings.getOne.queryOptions({ id: meetingId })
   );
+
+  if (data.status === "completed") {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <ErrorState
+          title="Meeting has ended"
+          description="You can no longer join this meeting"
+        />
+      </div>
+    );
+  }
 
   return <div>{JSON.stringify(data, null, 2)}</div>;
 };
