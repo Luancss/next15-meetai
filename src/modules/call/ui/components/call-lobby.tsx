d0b024eddd1be7beaa2eca6872b3fc23e7f76859
Luan <ventobo123@gmail.com>
2025-10-06 18:09:24 -0300

    feat: integrate CallLobby component into CallUI for improved call management

--- src/modules/call/ui/components/call-lobby.tsx
+++ src/modules/call/ui/components/call-lobby.tsx
@@ -5,23 +5,23 @@ interface Props {
 }
 
 export const CallLobby = ({ onJoin }: Props) => {
-    const {useCameraState, useMicrophoneState} = useCallStateHooks();
+  const { useCameraState, useMicrophoneState } = useCallStateHooks();
 
-    const {hasBrowserPermission: hasMicPermissions} = useMicrophoneState();
-    const {hasBrowserPermission: hasCameraPermissions} = useCameraState();
+  const { hasBrowserPermission: hasMicPermissions } = useMicrophoneState();
+  const { hasBrowserPermission: hasCameraPermissions } = useCameraState();
 
-    const hasBrowserMediaPermissions = hasMicPermissions && hasCameraPermissions;
+  const hasBrowserMediaPermissions = hasMicPermissions && hasCameraPermissions;
 
   return (
-      <div className="flex flex-col items-center justify-center h-full bg-radial from-sidebar-accent to-sidebar">
-        <div className="py-4 px-8 flex flex-1 items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-y-6 bg-bkacjground rounded-lg p-10 shadow-sm">
-    <div className="flex flex-col gap-y-2 text-center">
-    <h6>Ready to join?</h6>
-    </div>
-      </div>
+    <div className="flex flex-col items-center justify-center h-full bg-radial from-sidebar-accent to-sidebar">
+      <div className="py-4 px-8 flex flex-1 items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-y-6 bg-bkacjground rounded-lg p-10 shadow-sm">
+          <div className="flex flex-col gap-y-2 text-center">
+            <h6 className="text-lg font-medium">Ready to join?</h6>
+            <p className="text-sm">Set up your call before joining</p>
+          </div>
         </div>
       </div>
-
-  )
-}
+    </div>
+  );
+};
